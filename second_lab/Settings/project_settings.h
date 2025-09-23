28424880525ca7df4f50de9a6ffec788b670ece8
Artemdjdj <artembasuraa@gmail.com>
2025-09-23 04:57:22 +0300

    deploy version settings docs

--- second_lab/Settings/project_settings.h
+++ second_lab/Settings/project_settings.h
@@ -6,8 +6,11 @@
 #define NAME_OF_FILE_LOGS_GENERATE_ID "C:/Users/Redmi/Desktop/ppois/second_lab/Logs/generator_id_log.txt"
 #include <cmath>
 #include <string>
-constexpr int length_of_id = 8;
-constexpr int max_count_of_attempts = pow(10, 8);
+constexpr int length_of_id = 8;/*!< Длина id */;
+constexpr int max_count_of_attempts = pow(10, 8);/*!< Количество уникальных id */;
 
+/*! \brief Очитска файла логирования, если он существует
+	*  \return Число 0 если все прошло успешно, 1 в обратном случае
+	*/
 int CheckFileLog(std::string file_name);
 #endif
