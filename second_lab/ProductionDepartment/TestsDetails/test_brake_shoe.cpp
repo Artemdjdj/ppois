13a475477850355b620fcfb64e4db2a925253ac5
Artemdjdj <artembasuraa@gmail.com>
2025-10-29 20:53:48 +0300

    Test color in class BrakeShoe

--- second_lab/ProductionDepartment/TestsDetails/test_brake_shoe.cpp
+++ second_lab/ProductionDepartment/TestsDetails/test_brake_shoe.cpp
@@ -5,64 +5,79 @@
 
 class TestingBrakeShoe : public ::testing::Test {
 protected:
-	void SetUp() override {
-		material = "low-metal";
-		brake_shoe = BrakeShoe(12, 25, "ceramic", color);
-		brake_shoe.SetMaterial(material);
-	}
-	Color color = Color("blue");
-	std::string material;
-	BrakeShoe brake_shoe;
+    void SetUp() override {
+        material = "low-metal";
+        brake_shoe = BrakeShoe(12, 25, "ceramic", color);
+        brake_shoe.SetMaterial(material);
+    }
+
+    Color color = Color("blue");
+    std::string material;
+    BrakeShoe brake_shoe;
 };
 
 TEST_F(TestingBrakeShoe, TestMaterialPositive) {
-	ASSERT_EQ("low-metal", brake_shoe.GetMaterial());
+    ASSERT_EQ("low-metal", brake_shoe.GetMaterial());
 }
 
 TEST_F(TestingBrakeShoe, TestMaterialNegative) {
-	ASSERT_THROW(
-		brake_shoe.SetMaterial("low-m"),
-		ExceptionIncorrectMaterial
-	);
-	ASSERT_EQ("low-metal", brake_shoe.GetMaterial());
+    ASSERT_THROW(
+        brake_shoe.SetMaterial("low-m"),
+        ExceptionIncorrectMaterial
+    );
+    ASSERT_EQ("low-metal", brake_shoe.GetMaterial());
 }
 
 TEST_F(TestingBrakeShoe, TestHeight) {
-	ASSERT_EQ(brake_shoe.GetHeight(), 12);
+    ASSERT_EQ(brake_shoe.GetHeight(), 12);
 }
 
 TEST_F(TestingBrakeShoe, TestWidth) {
-	ASSERT_EQ(brake_shoe.GetWidth(), 25);
+    ASSERT_EQ(brake_shoe.GetWidth(), 25);
 }
 
 TEST_F(TestingBrakeShoe, TestSetHeight) {
-	brake_shoe.SetHeight(56);
-	ASSERT_EQ(brake_shoe.GetHeight(), 56);
+    brake_shoe.SetHeight(56);
+    ASSERT_EQ(brake_shoe.GetHeight(), 56);
 }
 
 TEST_F(TestingBrakeShoe, TestSetWidth) {
-	brake_shoe.SetWidth(100);
-	ASSERT_EQ(brake_shoe.GetWidth(), 100);
+    brake_shoe.SetWidth(100);
+    ASSERT_EQ(brake_shoe.GetWidth(), 100);
+}
+
+TEST_F(TestingBrakeShoe, TestSetColor) {
+    const auto color = Color("green");
+    brake_shoe.SetColor(color);
+    ASSERT_EQ(brake_shoe.GetColor(), "green");
+}
+
+TEST_F(TestingBrakeShoe, TestSetIncorrectColor) {
+    ASSERT_THROW(
+        const auto color = Color("greensdsfsd");
+        brake_shoe.SetColor(color),
+        ExceptionIncorrectColor
+    );
 }
 
 TEST_F(TestingBrakeShoe, TestSetHeightNegative) {
-	ASSERT_THROW(
-		brake_shoe.SetHeight(-56),
-		ExceptionIncorrectSize
-	);
-	ASSERT_EQ(brake_shoe.GetHeight(), 12);
+    ASSERT_THROW(
+        brake_shoe.SetHeight(-56),
+        ExceptionIncorrectSize
+    );
+    ASSERT_EQ(brake_shoe.GetHeight(), 12);
 }
 
 TEST_F(TestingBrakeShoe, TestSetWidthNegative) {
-	ASSERT_THROW(
-		brake_shoe.SetWidth(-100),
-		ExceptionIncorrectSize
-	);
-	ASSERT_EQ(brake_shoe.GetWidth(), 25);
+    ASSERT_THROW(
+        brake_shoe.SetWidth(-100),
+        ExceptionIncorrectSize
+    );
+    ASSERT_EQ(brake_shoe.GetWidth(), 25);
 }
 
 TEST(TestBrakeShoe, TestBigMaterial) {
-	auto brake_shoe = BrakeShoe();
-	brake_shoe.SetMaterial("Low-MetAL");
-	ASSERT_EQ("low-metal", brake_shoe.GetMaterial());
+    auto brake_shoe = BrakeShoe();
+    brake_shoe.SetMaterial("Low-MetAL");
+    ASSERT_EQ("low-metal", brake_shoe.GetMaterial());
 }
